26c476b1c1fc8c73214e17e2f5ecbf41495909b0
Reno Sorensen <renojames19@gmail.com>
2019-01-14 00:12:00 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT title FROM series LEFT JOIN characters ON series.id = characters.series_id GROUP BY title LIMIT 1"   #THIS IS CHEATING
 end
 
 def select_character_names_and_number_of_books_they_are_in
